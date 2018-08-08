e8dd1b9a86df985db9051af28743a354a6ad9acd
e <1435236447@qq.com>
2018-08-08 17:22:26 +0800

    使用vue-cli脚手架实现todolist

---
+++ todolist/src/TodoList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div>
+      <input v-model="inputValue"/> 
+      <button @click="handclick">提交</button>
+    </div>
+    <div>
+      <ul>
+          <Todo-Item 
+              v-for="(item,index) of list"
+              :content="item"
+              :index="index"
+              @delete="handdelete"
+          ></Todo-Item>
+      </ul>
+    </div>
+
+  </div> 
+  
+</template>
+
+<script>
+//引入组件模板
+import TodoItem from './components/TodoItem'
+export default {
+  components:{
+    'Todo-Item':TodoItem
+  },
+  data:function(){
+        return {
+            inputValue:'',
+            list:[]
+        }
+  },
+
+  methods:{
+    handclick(){
+        this.list.push(this.inputValue);
+        this.inputValue='';
+    },
+    handdelete(index){
+        this.list.splice(index,1);
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ todolist/src/components/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <li @click="handdelet">{{content}}</li>
+
+</template>
+
+<script>
+export default {
+  props:['content','index'],
+  methods:{
+      handdelet(){
+        this.$emit('delete',this.index)
+      }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
